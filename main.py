38419b90df7453b865b45fe45d1c0e30ddb47bb3
kimsagha <kimiya.sagha@gmail.com>
2021-08-06 14:39:28 +0200

    completed classification model

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+
+global df
 
 
 def read_data():
-    print("Reading in data from car.data")
+    print("Reading in data from car.data...")
+    global df
     df = pd.read_csv('Data/car.data', sep=",", header=None)
     df.columns = ['buying', 'maint', 'doors', 'persons', 'lug_boot', 'safety', 'class']
     print("Showing the first 5 data points")
     print(df.head())
 
-    print("\nChecking data distribution")
+    print("\nChecking data distribution\n(Figure 1)")
     class_names = df['class'].unique()
     class_counts = df['class'].value_counts()
     x_pos = np.arange(len(class_names))
@@ -21,7 +26,34 @@ def read_data():
     plt.xticks(x_pos, class_names)
     plt.show()
 
-    print('Number of null values: ', df.isnull().sum().sum())
+    print('\nNumber of null values: ', df.isnull().sum().sum())
+
+    # numerically encoding strings (discrete values) in df
+    df['buying'] = df['buying'].map({'vhigh': 0, 'high': 1, 'med': 2, 'low': 3})
+    df['maint'] = df['maint'].map({'vhigh': 0, 'high': 1, 'med': 2, 'low': 3})
+    df['doors'] = df['doors'].map({'2': 0, '3': 1, '4': 2, '5more': 3})
+    df['persons'] = df['persons'].map({'2': 0, '4': 1, 'more': 2})
+    df['lug_boot'] = df['lug_boot'].map({'small': 0, 'med': 1, 'big': 2})
+    df['safety'] = df['safety'].map({'low': 0, 'med': 1, 'high': 2})
+    df['class'] = df['class'].map({'unacc': 0, 'acc': 1, 'good': 2, 'vgood': 3})
+    print('\nNumerically encoded dataset:\n', df.head())
+
+
+def run_model():
+    # split dataset into 70% training data and 30% test data (randomly)
+    x_train, x_test, y_train, y_test = train_test_split(df[df.columns[:6]], df['class'], test_size=0.3, random_state=0)
+
+    # create and train classification model using logistic regression algorithm
+    c_model = LogisticRegression(max_iter=500, random_state=0)
+    c_model.fit(x_train, y_train)
+
+    # run model
+    # predictions = c_model.predict(x_test)
+
+    # get model accuracy
+    score = c_model.score(x_test, y_test)
+    print('score', score)
 
 
 read_data()
+run_model()
